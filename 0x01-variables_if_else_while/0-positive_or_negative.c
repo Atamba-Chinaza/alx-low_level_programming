ba10d6ceb62c5e88665c660e22f865000455cbaf
Atamba-Chinaza <atambanazzy@gmail.com>
2022-09-09 17:12:33 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - print the sting in put function
+ * Description: using the main function
+ * this program print "if statement of n value randomly picked"
+ * Return: 0
  */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
-	n =rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	if (n == 0)
-		printf("%d is zero\n", n);
+	n = rand() - RAND_MAX / 2;
+
 	if (n < 0)
+	{
 		printf("%d is negative\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
 	return (0);
 }
