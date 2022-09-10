5e259dc430011a044c1f4f9b511499fb2e315646
Atamba-Chinaza <atambanazzy@gmail.com>
2022-09-09 20:14:24 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,13 @@ int main(void)
 {
 	int a;
 
-	for (a = '0';a <= '9';a++)
+	for (a = '0'; a <= '9'; a++)
 	{
 		putchar(a);
 		if (a != '9')
 		{
 			putchar(',');
-			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
