2199583d391ce25c1d1a45050174532708e75ed5
Atamba-Chinaza <atambanazzy@gmail.com>
2022-10-20 17:22:04 -0700

    All task

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
  * add_node_end - adds a new node at the end
  * of a list_t list.
@@ -7,6 +7,7 @@
  * @str: string to store in the list.
  * Return: address of the head.
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
     list_t *new, *temp;
