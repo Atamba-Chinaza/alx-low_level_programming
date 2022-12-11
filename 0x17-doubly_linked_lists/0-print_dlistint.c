ca9d7299c066dc2315132c09e48b58e96033bbc0
Atamba-Chinaza <atambanazzy@gmail.com>
2022-12-10 23:52:19 -0800

    0 task

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -15,16 +15,16 @@ size_t print_dlistint(const dlistint_t *h)
 	count = 0;
 
 	if (h == NULL)
-		return (count);
+	return (count);
 
 	while (h->prev != NULL)
-		h = h->prev;
+	h = h->prev;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
-		count++;
-		h = h->next;
+	printf("%d\n", h->n);
+	count++;
+	h = h->next;
 	}
 
 	return (count);
